8df5551ac58f8f341bb6337ff9beaea56e33c3b8
GuiFritis <Guilhermecfritis@gmail.com>
2021-11-14 15:05:00 -0300

    add jump to player

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -9,11 +9,16 @@ public class Player : MonoBehaviour
     [SerializeField]
     private Rigidbody2D rigidbody2d;
 
+    private Vector2 friction = new Vector2(.1f, 0);
+
     public float speed;
+
+    public float jumpForce = 20f;
 
     // Update is called once per frame
     void Update()
     {
+        Jump();
         Move();
     }
 
@@ -24,6 +29,18 @@ public class Player : MonoBehaviour
         } else if(Input.GetKey(KeyCode.LeftArrow)){
             // rigidbody2d.MovePosition(rigidbody2d.position - velocity * Time.deltaTime);
             rigidbody2d.velocity = new Vector2(-speed, rigidbody2d.velocity.y);
+        }
+
+        if(rigidbody2d.velocity.x > 0){
+            rigidbody2d.velocity -= friction;
+        } else if (rigidbody2d.velocity.x < 0){
+            rigidbody2d.velocity += friction;
+        }
+    }
+
+    private void Jump(){
+        if(Input.GetKeyDown(KeyCode.Space)){
+            rigidbody2d.velocity = Vector2.up * jumpForce;
         }
     }
 }
