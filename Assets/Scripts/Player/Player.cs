0f6903adfc89a245002441dad6ec5446ce8c8831
GuiFritis <Guilhermecfritis@gmail.com>
2021-11-15 15:29:49 -0300

    add animations to player
    Add jump animation, landing animation and on damage taken animations to player

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class Player : MonoBehaviour
@@ -9,13 +10,31 @@ public class Player : MonoBehaviour
     [SerializeField]
     private Rigidbody2D rigidbody2d;
 
-    private Vector2 friction = new Vector2(.1f, 0);
+    [SerializeField]
+    private SpriteRenderer sprite;
 
+    [Header("Speed Setup")]
+    [SerializeField]
+    private Vector2 friction = new Vector2(.1f, 0);
     public float speed;
     public float runSpeedMultplier;
 
+    [Header("Jump Setup")]
     public float jumpForce = 20f;
 
+    [Header("Jump Animation Setup")]
+    public float jumpScaleY = 1.5f;
+    public float jumpScaleX = .7f;
+    public float jumpAnimationDur = .2f;
+    public float landAnimationDur = .2f;
+    public Ease jumpEase = Ease.OutBack;
+
+    [Header("Damage")]
+    public float dmgDur = .2f;
+    public Ease dmgEase = Ease.InBack;
+
+    private bool grounded = true;
+    private bool doubleJumped = false;
     private float _curSpeed;
 
     // Update is called once per frame
@@ -45,8 +64,46 @@ public class Player : MonoBehaviour
     }
 
     private void Jump(){
-        if(Input.GetKeyDown(KeyCode.Space)){
+        if(Input.GetKeyDown(KeyCode.Space) && (grounded || !doubleJumped)){
+            DOTween.Kill(rigidbody2d.transform);
+            rigidbody2d.transform.localScale = Vector2.one;
+
+            AnimateJump();
+
             rigidbody2d.velocity = Vector2.up * jumpForce;
+
+            if(grounded){
+                grounded = false;
+            } else {
+                doubleJumped = true;
+            }
         }
     }
+
+    private void AnimateJump(){
+        rigidbody2d.transform.DOScaleY(jumpScaleY, jumpAnimationDur).SetLoops(2, LoopType.Yoyo).SetEase(jumpEase);
+        rigidbody2d.transform.DOScaleX(jumpScaleX, jumpAnimationDur).SetLoops(2, LoopType.Yoyo).SetEase(jumpEase);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Ground") && !grounded){
+            DOTween.Kill(rigidbody2d.transform);
+            rigidbody2d.transform.localScale = Vector2.one;
+            AnimateLanding();
+            grounded = true;
+            doubleJumped = false;
+        }
+    }
+
+    private void AnimateLanding(){
+        rigidbody2d.transform.DOScaleY(jumpScaleX, landAnimationDur).SetLoops(2, LoopType.Yoyo).SetEase(jumpEase);
+        rigidbody2d.transform.DOScaleX(jumpScaleY, landAnimationDur).SetLoops(2, LoopType.Yoyo).SetEase(jumpEase);
+    }
+
+    public void onDamageTaken(){
+        DOTween.Kill(sprite);
+        sprite.DOColor(Color.red, dmgDur).SetEase(dmgEase).From();
+    }
+
 }
