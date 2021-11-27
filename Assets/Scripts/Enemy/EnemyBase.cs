f8a18df097099e70d528265f1614a303bcb44808
GuiFritis <Guilhermecfritis@gmail.com>
2021-11-27 15:35:43 -0300

    add animation to the enemy

--- Assets/Scripts/Enemy/EnemyBase.cs
+++ Assets/Scripts/Enemy/EnemyBase.cs
@@ -6,12 +6,21 @@ public class EnemyBase : MonoBehaviour
 {
     public int damage = 1;
 
+    public Animator animator;
+
+    public string triggerAtack = "Attack";
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         var healthBase = collision.gameObject.GetComponent<HealthBase>();
 
         if(healthBase != null){
             healthBase.TakeDamage(damage);
+            PlayAttackAnimation();
         }
+    }
+
+    private void PlayAttackAnimation(){
+        animator.SetTrigger(triggerAtack);
     }
 }
