2e76cbb0234845b594dff4d1594a0b84f3bb667b
GuiFritis <Guilhermecfritis@gmail.com>
2021-12-01 21:35:10 -0300

    add update to coins score

--- Assets/Scripts/Collectable/CollectableManager.cs
+++ Assets/Scripts/Collectable/CollectableManager.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Padrao.Core.Singleton;
+using TMPro;
 
 public class CollectableManager : Singleton<CollectableManager>
 {
     public int coins;
+
+    public TextMeshProUGUI textCoins;
 
     void Start()
     {
@@ -15,9 +18,11 @@ public class CollectableManager : Singleton<CollectableManager>
     private void Reset()
     {
         coins = 0;
+        textCoins.text = "X" + coins;
     }
 
     public void AddCoin(int amount = 1){
         coins += amount;
+        textCoins.text = "X" + coins;
     }
 }
